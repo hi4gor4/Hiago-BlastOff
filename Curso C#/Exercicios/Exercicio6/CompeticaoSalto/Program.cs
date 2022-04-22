0f13378e5923c8b3e79f03df3706912c2b6d0e58
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-22 16:26:26 -0300

    Adicionando exercicio 6 - C#

---
+++ Curso C#/Exercicios/Exercicio6/CompeticaoSalto/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace CompeticaoSalto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("Competicao de salto");
+            string nome = RegistraNome();
+            double[] saltos = new double[5]; 
+            for(short i=0; i<5; i++){
+                saltos[i] = RegistraSalto(i);
+
+            }
+            Saltador saltador = new Saltador(nome, saltos);
+            Console.WriteLine($"Atleta:{saltador.GetNome()} ");
+            Console.WriteLine($"\nPrimeiro Salto: {saltador.GetSalto(1).ToString("F1")}m");
+            Console.WriteLine($"Segundo Salto: {saltador.GetSalto(2).ToString("F1")}m");
+            Console.WriteLine($"Terceiro Salto: {saltador.GetSalto(3).ToString("F1")}m");
+            Console.WriteLine($"Quarto Salto: {saltador.GetSalto(4).ToString("F1")}m");
+            Console.WriteLine($"Quinto Salto: {saltador.GetSalto(5).ToString("F1")}m");
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine($"Melhor Salto: {saltador.getMelhorSalto().ToString("F1")}m");
+            Console.WriteLine($"Pior Salto: {saltador.getPiorSalto().ToString("F1")}m");
+            Console.WriteLine($"Media dos demais saltos {saltador.GetSaltoMedia().ToString("F1")}m");
+
+            Console.WriteLine("Resultado final");
+            Console.WriteLine($"{saltador.GetNome()}: {saltador.GetSaltoMedia().ToString("F1")}m");
+
+            Console.WriteLine("\n\n\nPara encerrar o programa tecle 1, ou qualquer tecla para continuar");
+            ushort seletor = 0;
+            try{
+                seletor = ushort.Parse(Console.ReadLine());
+            }catch{
+                
+                Menu();
+            }
+            if(seletor ==1)
+                System.Environment.Exit(0);
+            Menu();
+        }
+        static string RegistraNome(){
+            Console.WriteLine("Favor insira seu nome");
+            string nome = "";
+            try{
+                nome = Console.ReadLine();
+            }catch{
+                Console.WriteLine("Impossivel definir seu nome, aperte qualquer tecla para tentar novamente");
+                Console.ReadKey();
+                return RegistraNome();
+            }
+            if(nome == null){
+                Console.WriteLine("Impossivel definir seu nome, aperte qualquer tecla para tentar novamente");
+                Console.ReadKey();
+                return RegistraNome();
+            }
+            Console.Clear();
+            return nome;
+        }
+        static double RegistraSalto(short num){
+            Console.Clear();
+            Console.WriteLine($"Favor insira a distancia do seu salto {num}");
+            double distancia= 0;
+            try{
+                distancia = double.Parse(Console.ReadLine());
+            }catch{
+                Console.WriteLine("Impossivel Definir a distancia, aperte qualquer tecla para tentar novamente");
+                Console.ReadKey();
+                return RegistraSalto(num);
+            }
+            if(distancia <=0){
+                Console.WriteLine("Impossivel Definir a distancia, aperte qualquer tecla para tentar novamente");
+                Console.ReadKey();
+                return RegistraSalto(num);
+            }
+            return distancia;
+        }
+
+    }
+}
---
+++ Curso C#/Exercicios/Exercicio6/CompeticaoSalto/Saltador.cs
@@ -0,0 +1,34 @@
+namespace CompeticaoSalto
+{
+    class Saltador
+    {
+        private string nome;
+        private double[] salto;
+        
+
+        public string GetNome(){
+            return this.nome;
+        }
+        public double GetSaltoMedia(){
+            Array.Sort(this.salto);
+            return (this.salto[1]+ this.salto[2]+this.salto[3])/3;
+        }
+        public double GetSalto(short num){
+            return this.salto[num-1];
+        }
+        public double getMelhorSalto(){
+            Array.Sort(this.salto);
+            return this.salto[0];
+        }
+ 
+        public double getPiorSalto(){
+            Array.Sort(this.salto);
+            return this.salto[4];
+        }
+
+        public  Saltador(string nome, double[] saltos){
+            this.salto = saltos;
+            this.nome = nome;
+        }
+    }
+}
