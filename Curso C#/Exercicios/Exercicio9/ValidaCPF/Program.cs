9685b1a78ff4a54c9d3077c6bb78656ca5140115
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-25 16:18:32 -0300

    :boar:Sistema de validacao de cpf - exercicio9 C#

---
+++ Curso C#/Exercicios/Exercicio9/ValidaCPF/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ValidaCPF // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu(){
+            while(true){
+                Console.Clear();
+                Console.WriteLine("Para verificar um cpf tecle          1");
+                Console.WriteLine("Para sair tecle                      2");
+                short opt = 0;
+                try{
+                    opt = short.Parse(Console.ReadLine());
+                }catch{
+                    Console.WriteLine("Favor insira uma opção valida, pressione qualquer tecla para prosseguir");
+                    Console.ReadKey();
+                    Menu();
+                }
+                if(opt == 1){
+                    PedeCpf();
+                }else if (opt ==2){
+                    Console.Clear();
+                    Environment.Exit(0);
+                }else{
+                    Console.WriteLine("Favor insira uma opção valida, pressione qualquer tecla para prosseguir");
+                    Console.ReadKey();
+                }
+            }    
+        }
+        static void PedeCpf(){
+            Console.Clear();
+            Console.WriteLine("Favor insira o cpf no formato: 000.000.000-00");
+            string cpf = Console.ReadLine();
+            Validador valid = new Validador();
+            if(cpf != null && valid.validaFormato(cpf) ){
+                if(valid.ValidaCPF(cpf)){
+                    Console.WriteLine($"O cpf {cpf} é valido");
+                }else{
+                    Console.WriteLine($"O cpf {cpf}  não é valido");
+                }
+                Console.WriteLine("Pressione qualquer tecla para continuar");
+            }else{
+                Console.WriteLine("O cpf não corresponte ao padrão desejado, pressione qualquer tecla para tentar novamente...");
+            
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Curso C#/Exercicios/Exercicio9/ValidaCPF/Validador.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+
+namespace ValidaCPF // Note: actual namespace depends on the project name.
+{
+
+    internal class Validador
+    {
+        private Regex verificador ;
+        public Validador(){
+            this.verificador = new Regex(@"^([0-9])([0-9])([0-9])[\.]([0-9])([0-9])([0-9])[\.]([0-9])([0-9])([0-9])-([0-9])([0-9])");
+        }    
+        public bool validaFormato(string cpf){
+            return this.verificador.IsMatch(cpf);
+        }
+        public bool ValidaCPF(string cpf){
+            if(verificador.IsMatch(cpf)){
+                var encontrada = verificador.Match(cpf); 
+                return VerificaDigito(1,encontrada) && VerificaDigito(2,encontrada);
+            }else{
+                return false;
+            }
+        }
+        private bool VerificaDigito(short num, Match digitos){
+            int somatorio = 0;
+            int j = 10;
+            for(int i = num ; i <=num+8; i++){
+                somatorio += int.Parse(digitos.Groups[i].Value)*j;
+                j--;
+            }
+            int resto = somatorio%11;
+            int digitoVerificado = 0;
+            if(resto != 0 && resto != 1)
+                digitoVerificado = 11-resto;
+
+            return digitoVerificado == int.Parse(digitos.Groups[num + 9].Value);
+        }
+    }
+}
