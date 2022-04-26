9791e380e652f87ca1c7a1245d0ff8dcc98d9315
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-26 13:53:23 -0300

    :boar: Adicionando exemplos de emcapsulamento Balta

---
+++ Fundamentos de POO - Balta/Encapsulamento - Classe - Objeto - Abstração/ModeloEncapsulamento/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cst  = new Customer("Hiago"); 
+        }
+    }
+    internal class Customer{// classe são valores de referencia --- composto de propriedades metodos e eventos
+        //->Endereço dos dados
+
+        private string nome;
+        public Customer(string nome){
+            this.nome= nome;
+        }
+
+    }
+}
