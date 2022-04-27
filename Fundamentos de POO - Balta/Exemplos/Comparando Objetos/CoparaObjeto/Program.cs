8020a3d69bd4580dff0116cd86edeb0ed7014b95
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-27 16:13:41 -0300

    :boar: Adicionando exemplos de comparação de objetos

---
+++ Fundamentos de POO - Balta/Exemplos/Comparando Objetos/CoparaObjeto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp{
+    internal class Program{
+        static void main(string[] args){
+            var pessoaUm =new Pessoa(1);
+            var pessoaDois = new Pessoa(1);
+            Console.WriteLine(pessoaDois== pessoaUm);//false pois compara os endereços
+            Console.WriteLine(pessoaUm.Equals(pessoaDois));//true pois compara os objetos
+        }
+    }
+}
