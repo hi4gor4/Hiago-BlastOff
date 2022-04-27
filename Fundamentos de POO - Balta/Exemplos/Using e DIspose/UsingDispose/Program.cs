87ee39f7ecaef115e4bf8f12db77070b17176d15
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-27 11:14:22 -0300

    :boar: Exemplo de uso do using e do dispose

---
+++ Fundamentos de POO - Balta/Exemplos/Using e DIspose/UsingDispose/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var pag = new Pagamento();
+            pag.Dispose();//dispose manual
+            using(var pag2 = new Pagamento()){
+                //O bloco de using permite o auto dispose em classes que implementam o IDisposable
+            }
+        }
+    }
+}
