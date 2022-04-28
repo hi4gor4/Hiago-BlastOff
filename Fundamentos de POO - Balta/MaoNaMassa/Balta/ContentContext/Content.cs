036c853b9e0802d77871e2e4ebcb4b618f2dd28e
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-28 16:23:42 -0300

    Adicionado mão na massa Balta

---
+++ Fundamentos de POO - Balta/MaoNaMassa/Balta/ContentContext/Content.cs
@@ -0,0 +1,19 @@
+
+using System.Runtime.InteropServices;
+namespace Balta.ContentContext
+{
+    public abstract class Content
+    {
+        public Content(string title, string url)
+        {
+            Title = title;
+            Url = url;
+            Id =  Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+
+
+    }
+}
---
+++ Fundamentos de POO - Balta/MaoNaMassa/Balta/ContentContext/Enums/EContentLevel.cs
@@ -0,0 +1,9 @@
+namespace Balta.ContentContext.Enums
+{
+    public enum EContentLevel
+    {
+        Beginner = 1,
+        Fundamental = 2,
+        Advanced = 3,
+    }
+}
