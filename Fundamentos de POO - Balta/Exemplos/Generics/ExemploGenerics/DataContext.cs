8181bf8a5d46c30c38c925dbd420aac8e58bfefa
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-28 13:17:03 -0300

    :boar: Adicionando exemplo generics C#

--- Fundamentos de POO - Balta/Exemplos/Generics/ExemploGenerics/DataContext.cs
+++ Fundamentos de POO - Balta/Exemplos/Generics/ExemploGenerics/DataContext.cs
@@ -1,8 +1,9 @@
 namespace MyApp
 {
-    public class DataContext<T>// <> permite a implementação de uma classe ao model ogenerico
+    public class DataContext<P>// <> permite a implementação de uma classe ao model ogenerico // pode receber mais que um genrics 
+    where P : Person// diz que P só pode ser um Person
     {
-        public void Save(T entity){//quando recebendo uma entidade valida com a declaração pode ser usado
+        public void Save(P entity){//quando recebendo uma entidade valida com a declaração pode ser usado
             Console.WriteLine("Salvo com sucesso");
         }
 
