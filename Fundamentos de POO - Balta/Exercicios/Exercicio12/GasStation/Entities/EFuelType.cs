6881b49d16aa512689b6ef8ea444c92dbfdf13a7
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-04-29 15:10:46 -0300

    :boar: Exercicio Modelagem Bomba de Combustivel

---
+++ Fundamentos de POO - Balta/Exercicios/Exercicio12/GasStation/Entities/EFuelType.cs
@@ -0,0 +1,10 @@
+namespace FuelStation
+{
+    public enum EFuelType
+    {
+        Gasoline = 1,
+        Diesel = 2,
+        Ethanol = 3,
+
+    }
+}
---
+++ Fundamentos de POO - Balta/Exercicios/Exercicio12/GasStation/Entities/FuelPump.cs
@@ -0,0 +1,47 @@
+namespace FuelStation
+{
+    public class FuelPump
+    {
+        public FuelPump(double valueLiter, double tank, EFuelType fuelType)
+        {
+            ValueLiter = valueLiter;
+            Tank = tank;
+            FuelType = fuelType;
+        }
+
+        public double ValueLiter { get; set; }
+        public double  Tank { get; set; }
+        public EFuelType FuelType { get; set; }
+
+        public bool SupplyByValue(double value){
+            if(value/this.ValueLiter <= Tank){
+                Tank -= (value/ValueLiter);
+                return true;
+            }
+            return false;
+        }
+        public bool SupplyByLiter(double value){
+            if(value <= Tank){
+                Tank -= value;
+                return true;
+            }
+            return false;
+        }
+        public void ChangeLiterValue(double literValue){
+            this.ValueLiter = literValue;
+        }
+
+        public void ChangeFuelType(EFuelType fuelType){
+            this.FuelType = fuelType;
+        }
+
+        public bool ChangeTank(double fuel){
+            if(Tank + fuel > 0 ){
+                Tank +=fuel;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
