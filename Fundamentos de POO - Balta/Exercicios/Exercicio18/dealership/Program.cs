d5cbd550efdf3545c5565b9183bd1b2ed38fa962
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-05-10 10:39:39 -0300

    :boar: adicção do sitema de compra de automoveis premium

--- Fundamentos de POO - Balta/Exercicios/Exercicio18/dealership/Program.cs
+++ Fundamentos de POO - Balta/Exercicios/Exercicio18/dealership/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using DealerShipEntities;
 using DealershipEnums;
 using UserSolicitor;
 namespace DealershipControllers
@@ -15,8 +16,10 @@ namespace DealershipControllers
             switch(option)
             {
                 case 1:
+                    BuyAutomobile();
                     break;
                 case 2:
+                    BuyPremiumAutomobile();
                     break;
                 case 3:
                     Console.Clear();
@@ -25,16 +28,86 @@ namespace DealershipControllers
             }
         }
 
-        static void buyAutomobile()
+
+        static void BuyAutomobile()
         {
             Console.Clear();
-            Console.WriteLine("Vamos fazer o orsamento do seu veiculo");
+            Console.WriteLine("Vamos fazer o orçamento do seu veiculo");
             Console.WriteLine("Para isso preciso saber de alguns dados do seu carro");
             Console.WriteLine("Primeiro quero saber qual tipo de combustivel");
             byte fuel = GetFuelType();
+            Console.WriteLine("Prossiga escolhendo a cor do seu veiculo");
+            EColor color = GetEcolor();
+            Console.WriteLine("Insira o modelo co seu carro");
+            string model = Solicitor.GetValidString();
+            Console.WriteLine("Insira a placa do veiculo");
+            string licensePlate = Solicitor.GetValidString();//ANCHOR fazer regex pra placa
             Console.Clear();
+            Console.WriteLine("Parabens pela sua escolha!");
+            Automobile clientAutomobile = new Automobile(fuel, color, model, licensePlate);
+            Console.WriteLine($"O {clientAutomobile.Model} de placa {clientAutomobile.LicensePlate} pode ser seu por apenas {clientAutomobile.Cost()}");
+            Console.WriteLine("1-Para compralo");
+            Console.WriteLine("2 Para tentar outro carro");
+            byte option = Solicitor.GetByteInterval(1,2);
+            if(option==1){
+                Console.WriteLine("Parabéns pela escolha pressione qualquer tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+                System.Environment.Exit(0);
+            }else{
+                MenuDealerShip();
+            }
         }
 
+        static void BuyPremiumAutomobile()
+        {
+            Console.Clear();
+            Console.WriteLine("Vamos fazer o orçamento do seu veiculo");
+            Console.WriteLine("Para isso preciso saber de alguns dados do seu carro");
+            Console.WriteLine("Primeiro quero saber qual tipo de combustivel");
+            byte fuel = GetFuelType();
+            Console.WriteLine("Prossiga escolhendo a cor do seu veiculo");
+            EColor color = GetEcolor();
+            Console.WriteLine("Insira o modelo co seu carro");
+            string model = Solicitor.GetValidString();
+            Console.WriteLine("Insira a placa do veiculo");
+            string licensePlate = Solicitor.GetValidString();//ANCHOR fazer regex pra placa
+            Console.Clear();
+            AutomobileDeluxe clientAutomobile = new AutomobileDeluxe(fuel, color, model, licensePlate);
+            Console.WriteLine("Podemos incluir alguns adicionais no seu veiculo!");
+            Console.WriteLine("Deseja adicionar Direção Hidraulica no seu veiculo?");
+            Console.WriteLine("Digite 1 para sim e 2 para não");
+            byte aditional = Solicitor.GetByteInterval(1,2);
+            if(aditional== 1)
+                clientAutomobile.AirConditioning = true;
+            
+            Console.WriteLine("Deseja adicionar Direção Hidraulica no seu veiculo?");
+            Console.WriteLine("Digite 1 para sim e 2 para não");
+            aditional = Solicitor.GetByteInterval(1,2);
+            if(aditional ==1)
+                clientAutomobile.HydraulicSteering= true;
+
+            Console.WriteLine("Deseja adicionar Vidros eletricos no seu veiculo?");
+            Console.WriteLine("Digite 1 para sim e 2 para não");
+            aditional = Solicitor.GetByteInterval(1,2);
+            if(aditional ==1)
+                clientAutomobile.ElectricGlass= true; 
+            Console.Clear();   
+            Console.WriteLine("Parabens pela sua escolha!");
+            Console.WriteLine($"O {clientAutomobile.Model} de placa {clientAutomobile.LicensePlate} pode ser seu por apenas {clientAutomobile.Cost()}");
+            Console.WriteLine("1-Para compralo");
+            Console.WriteLine("2 Para tentar outro carro");
+            byte option = Solicitor.GetByteInterval(1,2);
+            if(option==1){
+                Console.WriteLine("Parabéns pela escolha pressione qualquer tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+                System.Environment.Exit(0);
+            }else{
+                MenuDealerShip();
+            }
+        }
+        
         static byte GetFuelType()
         {
             Console.WriteLine("1-Para veiculos a GASOLINA");
@@ -42,8 +115,7 @@ namespace DealershipControllers
             Console.WriteLine("3-Para veiculos a DIESEL");
             Console.WriteLine("4-Para veiculos a GAS");
             return Solicitor.GetByteInterval(1,4); 
-            Console.WriteLine("Prossiga escolhendo a cor do seu veiculo");
-            EColor color = GetEcolor()
+
         }
         static EColor GetEcolor()
         {
@@ -54,5 +126,6 @@ namespace DealershipControllers
             Console.WriteLine("5-Para Prata");
             return (EColor) Solicitor.GetByteInterval(1,5);
         }
+        
     }
 }
