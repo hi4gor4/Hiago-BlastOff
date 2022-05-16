e5da5b17bd520c0053b3af279dddec9ff1750b82
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-05-16 09:10:13 -0300

    Menu estrutural de cadastro de corpos celestes

---
+++ Fundamentos de POO - Balta/Exercicios/Exercicio15/OuterSpace/Entities/OutherSpace.cs
@@ -0,0 +1,33 @@
+using OutherSpaceEnum;
+namespace OuterSpaceEntities
+{
+    public class OutherSpace
+    {
+        public OutherSpace()
+        {
+            Celestials = new List<CelestialBody>();
+        }
+        private List<CelestialBody> Celestials { get; set; }
+        
+        public void AddBody(CelestialBody item) => Celestials.Add(item);
+
+        public bool RemoveBody(int index){
+            if(Celestials[index] != null){
+                Celestials.Remove(Celestials[index]);
+                return true;
+            }
+            return false;
+        }
+        public int Count(ECelestialBodyType type)
+        {
+            int count = 0;
+            foreach(CelestialBody celestial in Celestials)
+            {
+                if(celestial.Type == type)
+                    count+=1;
+            }
+            return count;
+        }
+
+    }
+}
--- Fundamentos de POO - Balta/Exercicios/Exercicio15/OuterSpace/Program.cs
+++ Fundamentos de POO - Balta/Exercicios/Exercicio15/OuterSpace/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OuterSpaceEntities;
+using OutherSpaceEnum;
+using UserSolicitor;
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var space = new OutherSpace();
+            
+        }
+        static void Menu(OutherSpace space)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Bem vindo ao espcaço sideral");
+            Console.WriteLine($"Asteroids cadastrados: {space.Count(ECelestialBodyType.Asteroid)} ");
+            Console.WriteLine($"Planetas cadastrados : {space.Count(ECelestialBodyType.Planet)}   ");
+            Console.WriteLine($"Nebulosas cadastrados: {space.Count(ECelestialBodyType.Nebulae)}");
+            Console.WriteLine("1- Para cadastrar um novo corpo celeste");
+            Console.WriteLine("2- Para consular informações sobre os corpos cadastrados");
+            Console.WriteLine("3- Para Sair");
+            byte option = Solicitor.GetByteInterval(1,3);
+            switch(option)
+            {
+                case 1:
+                    //ANCHOR fazer função de cadastro
+                    break;
+                case 2: 
+                    //ANCHOR fazer função de consulta
+                    break;
+                case 3:
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+    }
+}
