5bee0e1c44b6bbe68491fe2b836e30d56b64241d
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-07 13:02:54 -0300

    Adicionando modelo de conexao

---
+++ Acesso a dados- Balta/Conection/conexaoteste/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=Balta;User ID=sa;Password=1q2w3e4r@#$";
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using(var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT * FROM CATEGORY";
+                var reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetString(1)}");
+                }
+
+            }
+            connection.Close();
+        }
+    }
+}
---
+++ Acesso a dados- Balta/balta/Exemplo/ConectDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+    class Program
+    {
+        static void Main(string[] ars)
+        {
+            const string connectionString  = "Server=localhost,1433;Database=Balta;User ID=sa;Password=1q2w3e4r@#$ ";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] from [Category]";
+                    SqlDataReader reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+                Console.WriteLine("Conectado");    
+            }
+            Console.WriteLine("Batata e pera");
+            
+            
+        }
+    }
