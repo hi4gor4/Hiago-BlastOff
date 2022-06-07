980fd72a1ced3a37d6a4ce5ae742d474d11f6984
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-07 14:49:06 -0300

    Adicionando modelo de insert com  dapper

--- Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
+++ Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
@@ -10,12 +10,40 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=Balta;User ID=sa;Password=1q2w3e4r@#$";
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Mqtt";
+            category.Url = "www.nada.com";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+            var insert = @"INSERT INTO 
+                [Category] 
+            VALUES(
+                @Id,
+                @Title,
+                @Url,
+                @Summary,
+                @Order,
+                @Description,
+                @Featured)";
             using(var connection = new SqlConnection(connectionString))
             {
+                var rows = connection.Execute(insert, new{
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine(rows);
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                foreach (var item in categories)
                 {
-                    Console.WriteLine($"{category.Title}, {category.Id}");
+                    Console.WriteLine($"{item.Title}, {item.Id}");
                 }
             }
         }
