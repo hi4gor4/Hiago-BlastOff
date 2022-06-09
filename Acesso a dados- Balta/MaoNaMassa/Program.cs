b6d529f12c4cba53531d89a64b959b2bf6b64c11
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-09 14:47:32 -0300

    Usando dapper contrib

---
+++ Acesso a dados- Balta/MaoNaMassa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string  CONNECTION_STRING =  @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING ))
+            {
+                var users = connection.GetAll<User>();
+                foreach(var user in users)
+                {
+                    Console.WriteLine(user.Email);
+                }
+            }
+        }
+    }
+}
