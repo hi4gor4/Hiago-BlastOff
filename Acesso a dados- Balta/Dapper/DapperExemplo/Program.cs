e77defa1c9c1659dbf59f11f621aad711083efbf
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-09 12:21:51 -0300

    Adicionando Like  com Dapper

--- Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
+++ Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
@@ -272,6 +272,68 @@ namespace MyApp // Note: actual namespace depends on the project name.
                         "e6730d1c-6870-4df3-ae68-438624e04c72"
                     }
                 });
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
             }
+            static void Like(SqlConnection connection, string term)
+        {
+            var query = @"SELECT * FROM [Course] WHERE [Title] LIKE @exp";
+
+            var items = connection.Query<Course>(query, new
+            {
+                exp = $"%{term}%"
+            });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Minha categoria que não";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }, transaction);
+
+                transaction.Commit();
+                // transaction.Rollback();
+
+                Console.WriteLine($"{rows} linhas inseridas");
+            }
+        }
     }
 }
