de7489a6722fb5d25a28f827d50cea6452869789
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-09 10:54:36 -0300

    Adicionando execute Scalar com Dapper

--- Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
+++ Acesso a dados- Balta/Dapper/DapperExemplo/Program.cs
@@ -116,7 +116,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void ExecuteReadProcedure(SqlConnection connection)
         {
             var procedure = "[spGetCourseByCategory]";
-            var parames = new = {CategoryId = "5d8cf24e-e717-9a02-2443-01b300000000"};
+            var parames = new  {CategoryId = "5d8cf24e-e717-9a02-2443-01b300000000"};
             var courses =  connection.Query(
                 procedure,
                 parames,
@@ -127,6 +127,40 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.WriteLine(item.Id);
             }
 
+        }
+        static void ExecuteScalar(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"
+                INSERT INTO 
+                    [Category] 
+                OUTPUT inserted.[Id]
+                VALUES(
+                    NEWID(), 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured) 
+                ";
+            var id = connection.ExecuteScalar<Guid>(insertSql, new
+            {
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"CCategoria inserida: {id}");
         }
     }
 }
