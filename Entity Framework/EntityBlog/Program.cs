972b76f290d54fb7c9a780898485630e0c5da8b8
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-13 11:30:25 -0300

    listando com datacontext

--- Entity Framework/EntityBlog/Program.cs
+++ Entity Framework/EntityBlog/Program.cs
@@ -10,12 +10,19 @@ namespace EntityBlog
         {
             using(var context = new BlogDataContext())
             {
-                // var tag = new Tag{ Name= "ASP", Slug= "/asp" };
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
-                var tag = context.Tags.FirstOrDefault(x=> x.Id ==1);
-                tag.Name = "Entity";
+                var adtag = new Tag{ Name= "ASP", Slug= "/asp" };
+                context.Tags.Add(adtag);
                 context.SaveChanges();
+                // var tag = context.Tags.FirstOrDefault(x=> x.Id ==1);
+                // tag.Name = "Entity";
+                // context.Update(tag);
+                // context.Remove(tag);
+                // context.SaveChanges();
+                var tags = context.Tags;
+                foreach(var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
             }
         }
     }
