4484738b3a60fe3033ad1f73fee06bb4cc217b88
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-14 10:42:42 -0300

    Use Include com ef

--- Entity Framework/EntityBlog - Modulo2/Program.cs
+++ Entity Framework/EntityBlog - Modulo2/Program.cs
@@ -10,32 +10,39 @@ namespace EntityBlog
         {
             using(var context = new BlogDataContext())
             {
-                var user = new User
-                {
-                    Name = "Hiago rRangel",
-                    Slug = "hiagora",
-                    Email = "hiago.toodoo",
-                    Bio = "blastofer",
-                    Image = "https://goo.com",
-                    PasswordHash = "123098457"
-                };
+                // var user = new User
+                // {
+                //     Name = "Hiago rRangel",
+                //     Slug = "hiagora",
+                //     Email = "hiago.toodoo",
+                //     Bio = "blastofer",
+                //     Image = "https://goo.com",
+                //     PasswordHash = "123098457"
+                // };
                 
-                var category = new Category {Name = "Backend", Slug = "backend"};
+                // var category = new Category {Name = "Backend", Slug = "backend"};
                 
-                var post = new Post
-                {
-                    Author = user,
-                    Category = category,
-                    Body = "<p>Hello world</p>",
-                    Slug = "comecando-com-ef-core",
-                    Summary = "Neste artigo vamos aprender EF core",
-                    Title = "Começando com EF Core",
-                    CreateDate = DateTime.Now,
-                    LastUpdateDate = DateTime.Now,
-                };
+                // var post = new Post
+                // {
+                //     Author = user,
+                //     Category = category,
+                //     Body = "<p>Hello world</p>",
+                //     Slug = "comecando-com-ef-core",
+                //     Summary = "Neste artigo vamos aprender EF core",
+                //     Title = "Começando com EF Core",
+                //     CreateDate = DateTime.Now,
+                //     LastUpdateDate = DateTime.Now,
+                // };
                 
-                context.Posts.Add(post); 
-                context.SaveChanges();
+                // context.Posts.Add(post); 
+                // context.SaveChanges();
+                var posts = context
+                .Posts
+                .AsNoTracking()
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+                foreach (var post in posts)
+                    Console.WriteLine(post.Title);
             }
             }
         }
