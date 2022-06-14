a049de41b0256a6dc429d92520aeb43a3f1e39e8
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-14 11:58:14 -0300

    Adicionando relacionamento em post, de um para muitos

--- Entity Framework/EntityBlog - Mapeamento Fluente/Models/Category.cs
+++ Entity Framework/EntityBlog - Mapeamento Fluente/Models/Category.cs
@@ -10,5 +10,7 @@ namespace EntityBlog.Models
         public string Name { get; set; }
 
         public string Slug { get; set; }
+        public IList<Post> Posts { get; set; }
+
     }
 }
--- Entity Framework/EntityBlog - Mapeamento Fluente/Models/User.cs
+++ Entity Framework/EntityBlog - Mapeamento Fluente/Models/User.cs
@@ -11,5 +11,9 @@ namespace EntityBlog.Models
         public string Bio { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
+        public IList<Post> Posts { get; set; }
+        public IList<Role> Roles { get; set; }
+
+
     }
 }
