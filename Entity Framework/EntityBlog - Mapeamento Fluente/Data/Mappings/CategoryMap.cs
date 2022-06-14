af5ffc03485400f73d921b121d425f11f565d235
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-14 11:28:41 -0300

    Adicionando fluent mapping na tabela Category

--- Entity Framework/EntityBlog - Mapeamento Fluente/Data/Mappings/CategoryMap.cs
+++ Entity Framework/EntityBlog - Mapeamento Fluente/Data/Mappings/CategoryMap.cs
@@ -9,6 +9,25 @@ namespace EntityBlog.Mappings
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.ToTable("Category");// define a tabela
+            
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=> x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x=> x.Name)// Definindo instruções fluentes
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnName("NVARCHAR")
+                .HasMaxLength(80);
+            
+            builder.Property(x=> x.Slug)// Definindo instruções fluentes
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnName("VARCHAR")
+                .HasMaxLength(80);
+            
         }
     }
 }
