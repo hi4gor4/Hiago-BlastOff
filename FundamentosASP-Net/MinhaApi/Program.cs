5e33722db867be412d990d6abb66d7d08607aa3c
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-15 11:25:13 -0300

    Adicionando parametros na requsição

--- FundamentosASP-Net/MinhaApi/Program.cs
+++ FundamentosASP-Net/MinhaApi/Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => {
-    return Results.Ok("Tudo certo na bahia, terra linda de meu Deus");
+app.MapGet("/{nome}", (string nome) => {
+    return Results.Ok($"Tudo certo na bahia {nome}, terra linda de meu Deus");
 });
 
 app.Run();
