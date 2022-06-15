ddb576c4e58d3c32d2e05856472d49f2382f0ff0
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-15 11:33:18 -0300

    Adicionando post com JSon

--- FundamentosASP-Net/MinhaApi/Program.cs
+++ FundamentosASP-Net/MinhaApi/Program.cs
@@ -1,8 +1,12 @@
+using MinhaApi;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/{nome}", (string nome) => {
     return Results.Ok($"Tudo certo na bahia {nome}, terra linda de meu Deus");
 });
-
+app.MapPost("/", (User user)=>{
+    return Results.Ok(user.Username);
+});
 app.Run();
