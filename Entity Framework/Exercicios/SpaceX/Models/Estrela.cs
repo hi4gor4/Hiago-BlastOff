988fa73a700dd0af1ecb7f75d80d82a1f0ec76ff
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-06-16 14:04:08 -0300

    adicionando modelagem do entity

---
+++ Entity Framework/Exercicios/SpaceX/Models/Estrela.cs
@@ -0,0 +1,14 @@
+namespace Spacex.Models
+{
+
+    public class Estrela
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Massa { get; set; }
+        public decimal Tamanho { get; set; }
+        public string  Tipo { get; set; }
+
+
+    }
+}
---
+++ Entity Framework/Exercicios/SpaceX/Models/Galaxia.cs
@@ -0,0 +1,14 @@
+namespace Spacex.Models
+{
+
+    public class Galaxia
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Massa { get; set; }
+        public decimal Tamanho { get; set; }
+        public string  Tipo { get; set; }
+
+
+    }
+}
