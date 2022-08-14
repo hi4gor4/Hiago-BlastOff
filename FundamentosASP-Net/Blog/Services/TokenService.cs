7057524e428ad8e8ea72badfdfb815e6b37ceb74
hi4gor4 <hiago.almeida@toodoo.com.br>
2022-08-14 20:14:00 -0300

    Generate tokens

--- FundamentosASP-Net/Blog/Services/TokenService.cs
+++ FundamentosASP-Net/Blog/Services/TokenService.cs
@@ -1,30 +1,27 @@
 using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using Blog.Models;
-using Blog;
-using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
+using System.Text;
+using Blog.Extensions;
+using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.Services;
+
 public class TokenService
 {
     public string GenerateToken(User user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.Name, value: "hiagorangel"),
-                new Claim(ClaimTypes.Role, value: "admin" ),
-                new Claim("Banana", value: "fruta")
-            }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
+
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),
-                SecurityAlgorithms.HmacSha256Signature
-            )
+                SecurityAlgorithms.HmacSha256Signature)
         };
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
